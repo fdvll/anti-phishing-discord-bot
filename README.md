9be72782473ff04dbf6ff3b0d19f1faebfeda9f3
firedevil6651 <ryanlb07@icloud.com>
2022-01-22 19:17:17 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Anti-Phishing-Bot
+A Discord bot that deletes phishing links from all text channels. It checks for new links every 5 hours from [Nikolaischunk's phishing link github](https://github.com/nikolaischunk/discord-phishing-links) and stores them in the 'domains.json' file.
+
+# Config
+Make sure you have the required libraries installed by running 'python -m pip install discord' and 'python -m pip install colorama'.
+Edit the 'main.py' file and put your bot token on line 5 and everything is finished.
---
+++ main.py
@@ -0,0 +1,51 @@
+import json, discord, requests, datetime
+from colorama import Fore, init
+from discord.ext import tasks
+
+token = 'TOKEN'
+
+init(autoreset=True)
+client = discord.Client()
+
+@tasks.loop(hours=5)
+async def updateList():
+    contents = requests.get("https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/domain-list.json").text
+    f = open('./domains.json', 'wb')
+    f.write(contents.encode())
+
+@client.event
+async def on_ready():
+    print(Fore.GREEN + 'Logged in as {0.user}'.format(client))
+    print(Fore.BLUE + 'Starting list updater...')
+    try:
+        updateList.start()
+    except Exception as e:
+        print(e)
+    print(Fore.GREEN + 'Finished getting links, bot is ready')
+
+@client.event
+async def on_message(message):
+    data = json.load(open('./domains.json'))
+    now = datetime.datetime.today()
+    date = now.strftime("%m:%d:%Y %H:%M:%S")
+
+    if message.author == client.user:
+        return
+
+    for _ in data['domains']:
+        if str(_) in message.content:
+            await message.delete()
+            print(Fore.GREEN + 'Deleted phishing link:')
+            print(Fore.BLUE + 'Time: ' + date)
+            print(Fore.BLUE + 'Message: ' + message.content)
+            print(Fore.BLUE + 'User: ' + str(message.author))
+            print(Fore.BLUE + 'User ID: ' + str(message.author.id))
+            print(Fore.BLUE + 'Channel ID: ' + str(message.channel.id))
+            break
+        else:
+            pass
+
+try:
+    client.run(token)
+except Exception as e:
+    print(e)
